7c45594bb9bf8eea626a1e5825fc9e546744100e
Fedor4994 <fedorsosnin04@gmail.com>
2022-09-29 18:03:24 +0300

    Все работает, но без хранилища

--- js/script.js
+++ js/script.js
@@ -1,7 +1,93 @@
 const addMessage = document.querySelector('.todo__message');
 const addButton = document.querySelector('.todo__add');
-const todo = document.querySelector('.todo__list');
-const deleteButton = document.querySelector('.');
+const tasksList = document.querySelector('.todo__list');
+const deleteButton = document.querySelector('.todo__taks-delete');
+
+const tasks = [];
+
+addButton.addEventListener('click', onAddButtonClick);
+
+function onAddButtonClick() {
+  const newTaskText = addMessage.value;
+  if (newTaskText) {
+    addTask(newTaskText);
+    addMessage.value = '';
+    tasksRender(tasks);
+  }
+}
+// Выполняется когда нажимаем по кнопке
+
+function addTask(text) {
+  const task = {
+    id: Date.now(),
+    text,
+    isComplete: false,
+  };
+  // Создание обьекта одной добавляемой задачи
+
+  tasks.push(task);
+  // Добавление созданной задачи в массив задач
+}
+// Функция добавления задач
+
+function tasksRender(list) {
+  const html = list
+    .map(task => {
+      let cls = task.isComplete ? 'todo__task todo__task-complete' : 'todo__task';
+      // добавление класса выполненой задачи, если она выполнена
+      const checked = task.isComplete ? 'checked' : '';
+      // добавление атрибута поставленного чекбокса, если его поставили
+      return `<li id="${task.id}" class="${cls}">
+          <label class="todo__checkbox">
+            <input type="checkbox" ${checked} />
+            <div></div>
+          </label>
+          <span class="todo__task-text">${task.text}</span>
+          <img class="todo__taks-delete" src="./image/Vector.svg" alt="delete-btn" />
+        </li>`;
+    })
+    .join('');
+
+  tasksList.innerHTML = html;
+}
+
+tasksList.addEventListener('click', event => {
+  if (event.target.tagName === 'INPUT') {
+    const isComplete = event.target.checked;
+    const task = event.target.parentElement.parentElement;
+    const taskId = task.id;
+    changeTaskStatus(taskId, isComplete);
+    tasksRender(tasks);
+  }
+  // Реализовываем деелгирование событий. Вешаем слушатель клика на на весь список дел, но только когда клик приходится
+  // по чекбоксу - тогда выяняем выполнина ли задача
+
+  if (event.target.tagName === 'IMG') {
+    const task = event.target.parentElement;
+    const taskId = task.id;
+    deleteTaks(taskId);
+    tasksRender(tasks);
+  }
+});
+//Функция вывода списка задач
+
+function changeTaskStatus(id, status) {
+  tasks.forEach(task => {
+    if (task.id == id) {
+      task.isComplete = status;
+    }
+  });
+}
+// мееняем статус задачи
+
+function deleteTaks(id) {
+  tasks.forEach((task, index) => {
+    if (task.id == id) {
+      tasks.splice(index, 1);
+    }
+  });
+}
+
 // let addMessage = document.querySelector(".message"),
 //   addButton = document.querySelector(".add"),
 //   toDo = document.querySelector(".todo");
