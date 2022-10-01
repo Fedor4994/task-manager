9128b5b8cbfdcec1d093fa9ac25aa72ad5112855
Fedor4994 <fedorsosnin04@gmail.com>
2022-10-01 11:25:24 +0300

    Рефакторинг комментариев

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const deleteButton = document.querySelector('.todo__taks-delete');
 const deleteAllButton = document.querySelector('.delete-all-btn');
 const saveBtn = document.querySelector('.save__btn');
 
+// Если у нас есть кнопка добавления(мы находимся не в режиме редактирования), то
+// при нажатии Enter происходит добавление заметки
 window.addEventListener('keydown', sendAddMessage);
 function sendAddMessage(event) {
   if (addButton.textContent === '+' && event.code === 'Enter') {
@@ -14,59 +16,60 @@ function sendAddMessage(event) {
 
 let tasks = [];
 
+// Если мы открываем расширение и хотим там увидеть ранее записаные задачи
+// то в этом куске кода я достаю из хранилища массив обьектов(твоих задач)
+// и отрисовую их в разметке + записываю сразу в массив, потому что он при каждом открытии будет опусташаться
 if (localStorage.getItem('todo')) {
   tasksRender(JSON.parse(localStorage.getItem('todo')));
   tasks = JSON.parse(localStorage.getItem('todo')).slice(0);
 }
 
-// Если мы открываем расширение и хотим там увидеть ранее записаные задачи
-// то в этом куске кода я достаю из хранилища массив обьектов(твоих задач)
-// и отрисовую их в разметке + записываю сразу в массив, потому что он при каждом открытии будет опусташаться
-
-addButton.addEventListener('click', onAddButtonClick);
 // добавляю слушатель на кнопку добавления задачи
+addButton.addEventListener('click', onAddButtonClick);
 
+// Функционал кнопки удаления всех задач
 deleteAllButton.addEventListener('click', () => {
   localStorage.setItem('todo', '');
   tasks = [];
   tasksList.innerHTML = '';
   deleteAllButton.style.display = 'none';
 });
-// Функционал кнопки удаления всех задач
 
+// Выполняется когда нажимаем по кнопке
 function onAddButtonClick() {
   const newTaskText = addMessage.value;
   if (newTaskText) {
-    addTask(newTaskText);
     // добавляю новую задачу в массив + в хранилище
+    addTask(newTaskText);
     addMessage.value = '';
-    tasksRender(JSON.parse(localStorage.getItem('todo')));
     // рендерю раметку по данным из хранилища
+    tasksRender(JSON.parse(localStorage.getItem('todo')));
   }
 }
-// Выполняется когда нажимаем по кнопке
 
+// Функция добавления задач
 function addTask(text) {
+  // Создание обьекта одной добавляемой задачи
   const task = {
     id: Date.now(),
     text,
     isComplete: false,
   };
-  // Создание обьекта одной добавляемой задачи
 
+  // Добавление созданной задачи в массив задач и в хранилище
   tasks.push(task);
   localStorage.setItem('todo', JSON.stringify(tasks));
-  // Добавление созданной задачи в массив задач и в хранилище
 }
-// Функция добавления задач
 
+// Функция отрисовки задач на странице(из массива обьектов задач)
 function tasksRender(list) {
   const html = list
     .map(task => {
-      let cls = task.isComplete ? 'todo__task todo__task-complete' : 'todo__task';
       // добавление класса выполненой задачи, если она выполнена
-      const checked = task.isComplete ? 'checked' : '';
+      let cls = task.isComplete ? 'todo__task todo__task-complete' : 'todo__task';
       // добавление атрибута поставленного чекбокса, если его поставили
+      const checked = task.isComplete ? 'checked' : '';
+
       return `<li id="${task.id}" class="${cls}">
           <label class="todo__checkbox">
             <input type="checkbox" ${checked} />
@@ -79,18 +82,21 @@ function tasksRender(list) {
     })
     .join('');
 
-  tasksList.innerHTML = html;
   // суть отрисовки страницы, при добавлении каждой задачи, или изменении статуса задачи, они перерисовуются
+  tasksList.innerHTML = html;
 
+  // отображает кнопку удаления всего, если есть какая то задача в списке
   if (localStorage.getItem('todo') !== '[]') {
     deleteAllButton.style.display = 'block';
   } else {
     deleteAllButton.style.display = 'none';
   }
-  // отображает кнопку удаления всего, если есть какая то задача в списке
 }
 
+//Делегирование событий, для удаления\изменения статуса задач
 tasksList.addEventListener('click', event => {
+  // Реализовываем деелгирование событий. Вешаем слушатель клика на на весь список дел, но только когда клик приходится
+  // по чекбоксу - тогда выясняем выполнина ли задача(меняем статус отовсюду, и хранилище, и массив, и разметка)
   if (event.target.tagName === 'INPUT') {
     const isComplete = event.target.checked;
     const task = event.target.parentElement.parentElement;
@@ -99,9 +105,9 @@ tasksList.addEventListener('click', event => {
     localStorage.setItem('todo', JSON.stringify(tasks));
     tasksRender(JSON.parse(localStorage.getItem('todo')));
   }
-  // Реализовываем деелгирование событий. Вешаем слушатель клика на на весь список дел, но только когда клик приходится
-  // по чекбоксу - тогда выясняем выполнина ли задача(меняем статус отовсюду, и хранилище, и массив, и разметка)
 
+  // Реализовываем деелгирование событий. Вешаем слушатель клика на на весь список дел, но только когда клик приходится
+  // по кнопке удаления - тогда удаляем задачу(отовсюду, и хранилище, и массив, и разметка)
   if (event.target.classList.contains('todo__taks-delete')) {
     const task = event.target.parentElement;
     const taskId = task.id;
@@ -109,19 +115,17 @@ tasksList.addEventListener('click', event => {
     localStorage.setItem('todo', JSON.stringify(tasks));
     tasksRender(JSON.parse(localStorage.getItem('todo')));
   }
-  // Реализовываем деелгирование событий. Вешаем слушатель клика на на весь список дел, но только когда клик приходится
-  // по кнопке удаления - тогда удаляем задачу(отовсюду, и хранилище, и массив, и разметка)
 
+  // Реализовываем деелгирование событий. Вешаем слушатель клика на на весь список дел, но только когда клик приходится
+  // по кнопке редактирования - тогда редактируем задачу(отовсюду, и хранилище, и массив, и разметка)
   if (event.target.classList.contains('todo__task-edit')) {
     const task = event.target.parentElement;
     const taskId = task.id;
     editTask(taskId);
   }
-  // Реализовываем деелгирование событий. Вешаем слушатель клика на на весь список дел, но только когда клик приходится
-  // по кнопке редактирования - тогда редактируем задачу(отовсюду, и хранилище, и массив, и разметка)
 });
-//Делегирование событий, для удаления\изменения статуса задач
 
+// мееняем статус задачи в массиве
 function changeTaskStatus(id, status) {
   tasks.forEach(task => {
     if (task.id == id) {
@@ -129,8 +133,8 @@ function changeTaskStatus(id, status) {
     }
   });
 }
-// мееняем статус задачи в массиве
 
+// удаляем задачу из массива
 function deleteTaks(id) {
   tasks.forEach((task, index) => {
     if (task.id == id) {
@@ -138,30 +142,31 @@ function deleteTaks(id) {
     }
   });
 }
-// удаляем задачу из массива
 
+// редактируем задачу
 function editTask(id) {
   if (addMessage.value === '') {
+    // кнопочке сохранения и добавляения удалять и добавлять дисплей ноне, в зависимисти от ситуации
     addButton.style.display = 'none';
     addButton.textContent = '-';
     saveBtn.style.display = 'block';
-    // кнопочке сохранения и добавляения удалять и добавлять дисплей ноне, в зависимисти от ситуации
 
     tasks.forEach(task => {
       if (task.id == id) {
+        // Если мы находимся в режиме редактирования, то по нажатию Enter у нас происходит то же самое что
+        // и при нажатии saveBtn
         window.addEventListener('keydown', saveEditMessage);
         function saveEditMessage(event) {
           if (addButton.textContent === '-' && event.code === 'Enter') {
             editMessage();
           }
         }
-        // Если мы находимся в режиме редактирования, то по нажатию Enter у нас происходит то же самое что
-        // и при нажатии saveBtn
 
+        // Вкидываем в инпут текст выбранной для редактируемой задачи и фокусируемся на нем
         addMessage.value = task.text;
         addMessage.focus();
-        // Вкидываем в инпут текст выбранной для редактируемой задачи и фокусируемся на нем
 
+        // Когда нажимвем по кнопке сохранения, перерисовуем наш массив, с отредактировной таской
         saveBtn.addEventListener('click', editMessage);
         function editMessage() {
           task.text = addMessage.value;
@@ -173,9 +178,7 @@ function editTask(id) {
           addButton.style.display = 'block';
           addButton.textContent = '+';
         }
-        // Когда нажимвем по кнопке сохранения, перерисовуем наш массив, с отредактировной таской
       }
     });
   }
 }
-// редактируем задачу
